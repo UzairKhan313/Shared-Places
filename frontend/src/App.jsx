29479375225d92e56d2b504a19ce07915d0825d6
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-02-07 11:51:33 +0500

    Configure Basic Navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// Importing All Pages.
 import Users from './users/pages/Users'
+import NewPlace from './places/pages/NewPlace'
+import RouteErrorPage from './shared/pages/RouteErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Users />,
+    errorElement: <RouteErrorPage />,
+  },
+  {
+    path: 'new/place',
+    element: <NewPlace />,
+  },
+])
 
 function App() {
-  return <Users />
+  return <RouterProvider router={router} />
 }
 
 export default App
--- frontend/src/places/pages/NewPlace.jsx
+++ frontend/src/places/pages/NewPlace.jsx
@@ -1,1 +1,7 @@
-rafce
+import React from 'react'
+
+const NewPlace = () => {
+  return <div>NewPlace</div>
+}
+
+export default NewPlace
