3b01ab80e7bd6ceb841cf62583b40f448b26f8c1
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-02-17 15:51:19 +0500

    Implementating Basic routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const placeRoutes = require('./routes/Places-Routes')
 
 const app = express()
+
+app.use(placeRoutes)
 
 module.exports = app
