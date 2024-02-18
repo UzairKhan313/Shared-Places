c516862538252471eedb44248c6a881fe6a2da88
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-02-18 09:18:32 +0500

    Adding error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,16 @@ const placeRoutes = require('./routes/Places-Routes')
 
 const app = express()
 
-app.use(placeRoutes)
+app.use('/api/v1/places', placeRoutes)
+
+// Middle ware for Error. This is excute only when the above middle ware yield to error.
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error)
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || 'Something went wrong.' })
+})
 
 module.exports = app
--- backend/routes/Places-Routes.js
+++ backend/routes/Places-Routes.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const HttpError = require('../Model/http-error')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
+router.get('/:pid', (req, res, next) => {
+  // Getting a Place is from url
+  const placeId = req.params.pid
+  // This Place will be fetch from data base.
+  const place = null
+  if (!place) {
+    throw new HttpError("Couldn't found the place for provided place Id", 404)
+  }
   console.log('Places Routes')
   res.json({ message: 'Respond Send' })
+})
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid
+  const user = null
+  if (!user) {
+    return next(
+      new HttpError("Couldn't found the User for provided user Id.", 404)
+    )
+  }
 })
 
 module.exports = router
